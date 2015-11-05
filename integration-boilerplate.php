cea50da30c048c6ff8150044fba52b1abd40a887
jan <jan.s@icanlocalize.com>
2015-11-05 03:15:04 +0100

    Polishing
    layouts-383

--- integration-boilerplate.php
+++ integration-boilerplate.php
@@ -18,9 +18,16 @@ add_action( 'wpddl_theme_integration_support_ready', 'wpddl_integration_boilerpl
 
 /**
  * We need to continue only after the integration support has been loaded and we check the API version matches.
+ *
+ * This action should be fired at some point during the 'init' action.
+ *
+ * @param string $layouts_version
+ * @param int $integration_support_version
  * @todo This function name has to be unique. Use pattern "wpddl_integration_{$theme_name}_init".
  */
-function wpddl_integration_boilerplate_init( $layouts_version, $integration_support_version ) {
+function wpddl_integration_boilerplate_init(
+	/** @noinspection PhpUnusedParameterInspection */ $layouts_version, $integration_support_version )
+{
 	$supported_integration_api_version = 1;
 	if( $supported_integration_api_version == $integration_support_version ) {
 		require_once 'integration-loader.php';
