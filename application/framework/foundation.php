fccba349c6a3265fc7851456087fbebf16e510fd
riccardo <riccardo.s@icalocalize.com>
2015-11-23 21:39:40 +0100

    added filters to handle slider

--- application/framework/foundation.php
+++ application/framework/foundation.php
@@ -8,6 +8,11 @@ class WPDDL_Integration_Framework_Foundation extends WPDDL_Framework_Integration
         parent::__construct();
         do_action('ddl-integration_override_before_init', 'foundation', 'Foundation by ZURB');
         add_action( 'ddl-init_integration_override', array(&$this, 'addImageResponsiveSupport') );
+        add_action( 'wp_head', array(&$this, 'do_header') );
+    }
+
+    public function do_header(){
+        $this->print_favicon();
     }
 
     public function getColumnPrefix(){
@@ -23,6 +28,39 @@ class WPDDL_Integration_Framework_Foundation extends WPDDL_Framework_Integration
         add_filter( 'ddl-get_bs_thumbnail_gui', array(&$this, 'get_bs_thumbnail_gui') );
         add_filter( 'ddl-get_image_effects_gui', array(&$this, 'get_bs_thumbnail_gui') );
         add_filter('ddl-get_image_box_image_data_attributes', array(&$this, 'overrideImageData'), 99, 3);
+    }
+
+    public function addCarouselOverrides(){
+        add_filter( 'ddl-carousel_element_tag', array(&$this, 'carousel_element_tag') );
+        add_filter( 'ddl-carousel_elements_tag', array(&$this, 'carousel_elements_tag') );
+        add_filter( 'ddl-carousel_element_data_attribute', array(&$this, 'carousel_element_data_attribute') );
+        add_filter( 'ddl-carousel_container_class', array(&$this, 'carousel_container_class') );
+        add_filter('ddl-carousel_caption_class_attribute', array(&$this, 'carousel_caption_class_attribute'));
+        add_filter('ddl-get_carousel_indicators', array(&$this, 'get_carousel_indicators'));
+    }
+
+    public function carousel_element_tag(){
+            return 'ul';
+    }
+
+    public function carousel_elements_tag(){
+        return 'li';
+    }
+
+    public function carousel_element_data_attribute(){
+        return 'data-orbit';
+    }
+
+    public function carousel_container_class(){
+        return 'orbit-container';
+    }
+
+    public function carousel_caption_class_attribute(){
+        return 'orbit-caption';
+    }
+
+    public function get_carousel_indicators(){
+        return '';
     }
 
     public function get_thumbnail_class(){
@@ -46,5 +84,15 @@ class WPDDL_Integration_Framework_Foundation extends WPDDL_Framework_Integration
 
         $data_string .= 'data-interchange="['.$original[0].', (default)], ['.$large[0].', (large)], ['.$medium[0].', (medium)], ['.$thumbnail[0].', (small)]"';
         return $data_string;
+    }
+
+    private function print_favicon(){
+        $uri = WPDDL_CORNERSTONE_URI_FRAMEWORK . DIRECTORY_SEPARATOR . 'assets/images/favicons/';
+        ob_start();?>
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $uri; ?>favicon.ico">
+        <link rel="icon" type="image/png" sizes="96x96" href="<?php echo $uri; ?>favicon.ico">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $uri; ?>favicon.ico">
+        <?php
+        echo ob_get_clean();
     }
 }
