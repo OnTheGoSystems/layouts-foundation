92f76101fcca877ba7719bde316b48a477606cd1
riccardo <riccardo.s@icalocalize.com>
2016-01-04 13:44:45 +0100

    fixes issue with menu alignment

--- application/layouts/cell/view/site_title.php
+++ application/layouts/cell/view/site_title.php
@@ -1,6 +1,5 @@
-<ul class="title-area">
-	<li class="name">
-		<a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name');?>" rel="home"><?php bloginfo('name');?></a>
+<ul class="vertical large-horizontal dropdown menu" data-responsive-menu="drilldown large-dropdown">
+	<li class="menu-text">
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="show-for-large"><?php bloginfo( 'name' ); ?></a>
 	</li>
-	<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
 </ul>
--- application/layouts/row/cornerstone_header.php
+++ application/layouts/row/cornerstone_header.php
@@ -13,4 +13,53 @@ class WPDDL_Integration_Layouts_Row_Cornerstone_header extends WPDDL_Row_Type_Pr
 
         parent::setup();
     }
+
+    public function htmlOpen( $markup, $args ) {
+
+        if( $args['mode'] === $this->id ) {
+
+            $el_css = 'ddl-full-width-row ' . $args['row_class'] . $args['type'];
+
+            $css_classes = $this->getCssClasses();
+
+            $el_css .= ! empty( $css_classes )
+                ? ' ' . implode( $css_classes, ' ' )
+                : '';
+
+            $el_css .= isset( $args['additionalCssClasses'] )
+                ? ' '.$args['additionalCssClasses']
+                : '';
+
+            $el_id = isset( $args['cssId'] ) && ! empty( $args['cssId'] )
+                ? ' id="' . $args['cssId'] . '"'
+                : '';
+
+            ob_start();
+            echo '<nav class="' . $args['container_class'] . '">';
+            ?>
+
+            <div class="title-bar" data-responsive-toggle="top-menu" data-hide-for="large">
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="float-left"><?php bloginfo( 'name' ); ?></a>
+                <div class="float-right">
+                    <div class="title-bar-title">Menu</div>
+                    <button class="menu-icon" type="button" data-toggle></button>
+                </div>
+            </div>
+
+            <?php
+            echo '<' . $args['tag'] . $el_id . ' class="' . $el_css . '" '.$this->renderDataAttributes().'>';
+
+            $markup = ob_get_clean();
+        }
+
+        return $markup;
+    }
+
+    public function htmlClose( $output, $mode, $tag ) {
+        if( $mode === $this->id ) {
+            $output = '</' . $tag . '></nav>';
+        }
+
+        return $output;
+    }
 }
